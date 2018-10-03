f7c2ff8dfaff9a3539b92a6682d54a688481b59c
SevenSword <inabikari.rai@gmail.com>
2018-10-04 00:48:24 +0900

    通常再生するところまで

---
+++ Assets/Scripts/InteractiveLoopManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace InteractiveMusic
+{
+    public sealed class InteractiveLoopManager
+    {
+        public static InteractiveLoopManager Instance { get; } = new InteractiveLoopManager();
+
+        private InteractiveLoopManager()
+        {
+            // nop
+        }
+
+        public AudioSource Source { private get; set; }
+
+        public void Play()
+        {
+            this.CheckRegisterSource();
+
+            this.Source.Play();
+        }
+
+        private void CheckRegisterSource()
+        {
+            Assert.IsNotNull(this.Source);
+        }
+    }
+}
---
+++ Assets/Scripts/SampleScene.cs
@@ -0,0 +1,43 @@
+﻿using InteractiveMusic;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScene : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    enum LoopType
+    {
+        AllLoop,
+        Intro1,
+        Intro2,
+        Verse1_1,
+        Verse1_2,
+        Verse2_1,
+        Verse2_2,
+        Chorus
+    }
+
+    public void Start()
+    {
+        InteractiveLoopManager.Instance.Source = this.audioSource;
+    }
+
+    public void OnClick(int id)
+    {
+        var type = (LoopType)id;
+        Debug.LogFormat("LoopType: {0}", type);
+
+        if (type == LoopType.AllLoop)
+        {
+
+        }
+        else
+        {
+        }
+
+        InteractiveLoopManager.Instance.Play();
+    }
+}
