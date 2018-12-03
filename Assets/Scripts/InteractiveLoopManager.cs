cd3c15c4fe0966d3514d70ecca5fa605416f368a
SevenSword <inabikari.rai@gmail.com>
2018-12-03 19:26:15 +0900

    SuperLoop仮実装

--- Assets/Scripts/InteractiveLoopManager.cs
+++ Assets/Scripts/InteractiveLoopManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -19,12 +20,32 @@ namespace InteractiveMusic
 
         public AudioSource Source { private get; set; }
 
+        public float NowTime { get { return this.Source.time; } }
+
         private bool isPlaying = false;
         private bool isDisposed = false;
         private List<LoopData> loopDataList = new List<LoopData>();
         private int nextLoopId = -1;
         private int currentLoopId = -1;
         private LoopData? currentLoopData = null;
+
+        private List<SuperLoopData[]> SuperLoopDataList = new List<SuperLoopData[]>();
+
+        private bool isSuper = true;
+
+        /// <summary>
+        /// 遷移中フラグ
+        /// </summary>
+        private bool duringTrans = false;
+
+        /// <summary>
+        /// 遷移パート中かどうか
+        /// </summary>
+        private bool isTransPart = false;
+
+        private SuperLoopData? tempSuperLoopData = null;
+        private SuperLoopData normalLoopData;
+        private List<SuperLoopData> normalLoopList = new List<SuperLoopData>();
 
         public void Dispose()
         {
@@ -42,9 +63,17 @@ namespace InteractiveMusic
 
             if (!beforeIsPlaying)
             {
-                if (!this.currentLoopData.HasValue)
+                if (isSuper)
                 {
-                    this.currentLoopData = new LoopData(0.0f, this.Source.clip.length);
+                    this.currentLoopId = 0;
+                    this.nextLoopId = 0;
+                }
+                else
+                {
+                    if (!this.currentLoopData.HasValue)
+                    {
+                        this.currentLoopData = new LoopData(0.0f, this.Source.clip.length);
+                    }
                 }
 
                 this.CheckLoop();
@@ -59,12 +88,31 @@ namespace InteractiveMusic
         public void SetupLoopData(LoopData[] loopDataArray)
         {
             this.loopDataList = new List<LoopData>(loopDataArray);
+            this.isSuper = false;
+        }
+
+        public void SetupSuperLoopData(SuperLoopData[][] superLoopDataArray)
+        {
+            this.SuperLoopDataList.Clear();
+            foreach (var array in superLoopDataArray)
+            {
+                this.SuperLoopDataList.Add(array);
+            }
+            this.normalLoopData = this.SuperLoopDataList[0][0];
+            this.normalLoopList = this.SuperLoopDataList.SelectMany(v => v)
+                                                        .Where(v => !v.hasTransPart)
+                                                        .ToList();
+            Debug.Log(this.normalLoopList.Count());
+            this.isSuper = true;
         }
 
         public void ChangeLoop(int loopId)
         {
             Assert.IsTrue(loopId >= -1);
-            Assert.IsTrue(loopId <= this.loopDataList.Count);
+            if (!isSuper)
+            {
+                Assert.IsTrue(loopId <= this.loopDataList.Count);
+            }
 
             this.nextLoopId = loopId;
         }
@@ -80,31 +128,87 @@ namespace InteractiveMusic
                     this.Source.Play();
                 }
 
-
-                if (this.currentLoopId != this.nextLoopId)
+                if (this.isSuper)
                 {
-                    Debug.LogFormat("Change: {0} -> {1}", this.currentLoopId, this.nextLoopId);
-                    if (this.nextLoopId == NormalLoop)
+                    // 遷移中でも遷移パート中でも
+                    if (!this.duringTrans && !this.isTransPart)
                     {
-                        this.currentLoopId = NormalLoop;
-                        this.currentLoopData = new LoopData(0.0f, this.Source.clip.length);
+                        // 遷移処理
+                        if (this.currentLoopId != this.nextLoopId)
+                        {
+                            Debug.LogFormat("Change: {0} -> {1}", this.currentLoopId, this.nextLoopId);
+                            Debug.LogFormat("NowTime: {0}", this.Source.time);
+                            var superLoopDatas = this.SuperLoopDataList[this.currentLoopId].Where(x => x.NextLoopId == this.nextLoopId)
+                                                                                           .Where(x => x.StartTimeFromLoop <= this.Source.time && this.Source.time <= x.EndTimeFromLoop);
+                            Assert.IsTrue(superLoopDatas.Any());
+                            Assert.IsFalse(superLoopDatas.Count() != 1, superLoopDatas.Count().ToString());
+                            var superLoopData = superLoopDatas.FirstOrDefault();
+                            this.tempSuperLoopData = superLoopData;
+                            this.duringTrans = true;
+                            this.currentLoopId = this.nextLoopId;
+                        }
+                        else
+                        {
+                            // 通常ループ
+                            if (this.Source.time >= this.normalLoopData.EndTimeFromLoop)
+                            {
+                                this.Source.time = this.normalLoopData.StartTimeFromLoop;
+                            }
+                        }
                     }
-                    else
+
+                    // 遷移中（遷移パートへの遷移待ち）
+                    if (this.duringTrans)
                     {
-                        var buffer = this.Source.time - this.currentLoopData.Value.StartTime;
+                        Assert.IsTrue(this.tempSuperLoopData.HasValue);
+                        if (this.Source.time >= this.tempSuperLoopData.Value.EndTimeFromLoop)
+                        {
+                            this.duringTrans = false;
+                            this.isTransPart = true;
+                            this.Source.time = this.tempSuperLoopData.Value.StartTimeAtTarnsPart;
+                        }
+                    }
 
-                        this.currentLoopData = this.loopDataList[this.nextLoopId];
-                        this.Source.time = this.currentLoopData.Value.StartTime + buffer;
-
-                        this.currentLoopId = this.nextLoopId;
+                    // 遷移パート中（遷移パートの完了待ち）
+                    if (this.isTransPart)
+                    {
+                        Assert.IsTrue(this.tempSuperLoopData.HasValue);
+                        if (this.Source.time >= this.tempSuperLoopData.Value.EndTimeAtTransPart)
+                        {
+                            this.isTransPart = false;
+                            this.normalLoopData = this.normalLoopList[this.currentLoopId];
+                            this.Source.time = this.tempSuperLoopData.Value.DestinationTime;
+                            Debug.LogFormat("STFL: {0} ETFL: {1}", this.normalLoopData.StartTimeFromLoop, this.normalLoopData.EndTimeFromLoop);
+                        }
                     }
                 }
-
-                if (this.currentLoopData.HasValue)
+                else
                 {
-                    if (this.Source.time >= this.currentLoopData.Value.EndTime)
+                    if (this.currentLoopId != this.nextLoopId)
                     {
-                        this.Source.time = this.currentLoopData.Value.StartTime;
+                        Debug.LogFormat("Change: {0} -> {1}", this.currentLoopId, this.nextLoopId);
+                        if (this.nextLoopId == NormalLoop)
+                        {
+                            this.currentLoopId = NormalLoop;
+                            this.currentLoopData = new LoopData(0.0f, this.Source.clip.length);
+                        }
+                        else
+                        {
+                            var buffer = this.Source.time - this.currentLoopData.Value.StartTime;
+
+                            this.currentLoopData = this.loopDataList[this.nextLoopId];
+                            this.Source.time = this.currentLoopData.Value.StartTime + buffer;
+
+                            this.currentLoopId = this.nextLoopId;
+                        }
+                    }
+
+                    if (this.currentLoopData.HasValue)
+                    {
+                        if (this.Source.time >= this.currentLoopData.Value.EndTime)
+                        {
+                            this.Source.time = this.currentLoopData.Value.StartTime;
+                        }
                     }
                 }
 
@@ -132,6 +236,28 @@ namespace InteractiveMusic
             {
                 this.StartTime = st;
                 this.EndTime = et;
+            }
+        }
+
+        public struct SuperLoopData
+        {
+            public int NextLoopId;
+            public float StartTimeFromLoop;
+            public float EndTimeFromLoop;
+            public float StartTimeAtTarnsPart;
+            public float EndTimeAtTransPart;
+            public float DestinationTime;
+            public bool hasTransPart;
+
+            public SuperLoopData(int nli, float stfl, float etfl, float statp, float etatp, float dt, bool htp)
+            {
+                this.NextLoopId = nli;
+                this.StartTimeFromLoop = stfl;
+                this.EndTimeFromLoop = etfl;
+                this.StartTimeAtTarnsPart = statp;
+                this.EndTimeAtTransPart = etatp;
+                this.DestinationTime = dt;
+                this.hasTransPart = htp;
             }
         }
     }
--- Assets/Scripts/SampleScene.cs
+++ Assets/Scripts/SampleScene.cs
@@ -3,22 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SampleScene : MonoBehaviour {
 
     [SerializeField]
     private AudioSource audioSource;
+    [SerializeField]
+    private Text time;
 
     enum LoopType
     {
-        AllLoop = -1,
-        Intro1,
-        Intro2,
-        Verse1_1,
-        Verse1_2,
-        Verse2_1,
-        Verse2_2,
-        Chorus
+        START,
+        LEAD,
+        BEHIND,
+        LATER,
+        LETHAL,
+        FINAL
+    }
+
+    public void Update()
+    {
+        this.time.text = InteractiveLoopManager.Instance.NowTime.ToString();
     }
 
     public void Start()
@@ -26,14 +32,67 @@ public class SampleScene : MonoBehaviour {
         InteractiveLoopManager.Instance.Source = this.audioSource;
 
         // ループデータ設定
-        InteractiveLoopManager.Instance.SetupLoopData(new [] {
-            new InteractiveLoopManager.LoopData(0.0f, 14.45f),
-            new InteractiveLoopManager.LoopData(14.45f, 29.073f),
-            new InteractiveLoopManager.LoopData(29.073f, 43.524f),
-            new InteractiveLoopManager.LoopData(43.524f, 58.1476f),
-            new InteractiveLoopManager.LoopData(58.1476f, 72.4046f),
-            new InteractiveLoopManager.LoopData(72.6046f, 87.2216f),
-            new InteractiveLoopManager.LoopData(87.2216f, 101.691f)
+        // InteractiveLoopManager.Instance.SetupLoopData(new [] {
+        //     new InteractiveLoopManager.LoopData(0.0f, 14.45f),
+        //     new InteractiveLoopManager.LoopData(14.45f, 29.073f),
+        //     new InteractiveLoopManager.LoopData(29.073f, 43.524f),
+        //     new InteractiveLoopManager.LoopData(43.524f, 58.1476f),
+        //     new InteractiveLoopManager.LoopData(58.1476f, 72.4046f),
+        //     new InteractiveLoopManager.LoopData(72.6046f, 87.2216f),
+        //     new InteractiveLoopManager.LoopData(87.2216f, 101.691f)
+        // });
+        // スーパーループデータ設定
+        InteractiveLoopManager.SuperLoopData[] loop_one = {
+            new InteractiveLoopManager.SuperLoopData(0, 14.4f, 27.2f, 0.0f, 0.0f, 0.0f, false),
+
+            new InteractiveLoopManager.SuperLoopData(1, 24.0f, 27.2f, 30.4f, 32.0f, 92.8f, true),
+            new InteractiveLoopManager.SuperLoopData(1, 14.4f, 17.6f, 35.2f, 36.8f, 92.8f, true),
+            new InteractiveLoopManager.SuperLoopData(1, 17.6f, 20.8f, 40.0f, 41.6f, 92.8f, true),
+            new InteractiveLoopManager.SuperLoopData(1, 20.8f, 24.0f, 44.8f, 46.4f, 92.8f, true),
+
+            new InteractiveLoopManager.SuperLoopData(2, 24.0f, 27.2f, 30.4f, 32.0f, 172.8f, true),
+            new InteractiveLoopManager.SuperLoopData(2, 14.4f, 17.6f, 35.2f, 36.8f, 172.8f, true),
+            new InteractiveLoopManager.SuperLoopData(2, 17.6f, 20.8f, 40.0f, 41.6f, 172.8f, true),
+            new InteractiveLoopManager.SuperLoopData(2, 20.8f, 24.0f, 44.8f, 46.4f, 172.8f, true),
+
+            new InteractiveLoopManager.SuperLoopData(3, 24.0f, 27.2f, 30.4f, 32.0f, 251.2f, true),
+            new InteractiveLoopManager.SuperLoopData(3, 14.4f, 17.6f, 35.2f, 36.8f, 251.2f, true),
+            new InteractiveLoopManager.SuperLoopData(3, 17.6f, 20.8f, 40.0f, 41.6f, 251.2f, true),
+            new InteractiveLoopManager.SuperLoopData(3, 20.8f, 24.0f, 44.8f, 46.4f, 251.2f, true),
+
+            new InteractiveLoopManager.SuperLoopData(4, 24.0f, 27.2f, 30.4f, 32.0f, 331.2f, true),
+            new InteractiveLoopManager.SuperLoopData(4, 14.4f, 17.6f, 35.2f, 36.8f, 331.2f, true),
+            new InteractiveLoopManager.SuperLoopData(4, 17.6f, 20.8f, 40.0f, 41.6f, 331.2f, true),
+            new InteractiveLoopManager.SuperLoopData(4, 20.8f, 24.0f, 44.8f, 46.4f, 331.2f, true),
+        };
+
+        InteractiveLoopManager.SuperLoopData[] loop_two = {
+            new InteractiveLoopManager.SuperLoopData(1, 94.4f, 107.2f, 0.0f, 0.0f, 0.0f, false),
+        };
+
+        InteractiveLoopManager.SuperLoopData[] loop_three = {
+            new InteractiveLoopManager.SuperLoopData(2, 174.4f, 187.2f, 0.0f, 0.0f, 0.0f, false),
+        };
+
+        InteractiveLoopManager.SuperLoopData[] loop_four = {
+            new InteractiveLoopManager.SuperLoopData(3, 252.8f, 294.4f, 0.0f, 0.0f, 0.0f, false),
+        };
+
+        InteractiveLoopManager.SuperLoopData[] loop_five = {
+            new InteractiveLoopManager.SuperLoopData(4, 332.8f, 345.6f, 0.0f, 0.0f, 0.0f, false),
+        };
+
+        InteractiveLoopManager.SuperLoopData[] loop_six = {
+            new InteractiveLoopManager.SuperLoopData(5, 380.8f, 419.2f, 0.0f, 0.0f, 0.0f, false),
+        };
+
+        InteractiveLoopManager.Instance.SetupSuperLoopData(new InteractiveLoopManager.SuperLoopData[][]{
+            loop_one,
+            loop_two,
+            loop_three,
+            loop_four,
+            loop_five,
+            loop_six
         });
     }
 
@@ -50,5 +109,22 @@ public class SampleScene : MonoBehaviour {
         InteractiveLoopManager.Instance.ChangeLoop(id);
 
         InteractiveLoopManager.Instance.Play();
+    }
+
+    public void OnChangeBGM(int id)
+    {
+
+    }
+
+    public void OnChangeLoop(int id)
+    {
+        if (id == 0)
+        {
+            InteractiveLoopManager.Instance.Play();
+        }
+        else
+        {
+            InteractiveLoopManager.Instance.ChangeLoop(id);
+        }
     }
 }
